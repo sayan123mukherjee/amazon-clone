c4d159d4e58e7ebbf43b953f1ed7ecf2b4be0ffe
Sayan Mukherjee <smjee123@gmail.com>
2020-08-07 10:10:10 +0530

    Updates

---
+++ precache-manifest.39764135c55794157f8bf5dc08a75f98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07420e5e3d864b8565ce40b77a2c3d71",
+    "url": "/amazon-clone/index.html"
+  },
+  {
+    "revision": "85c66764f69da9ca7047",
+    "url": "/amazon-clone/static/css/main.1a1113c4.chunk.css"
+  },
+  {
+    "revision": "5deeb8c3cc7b3594a032",
+    "url": "/amazon-clone/static/js/2.e976ba49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/amazon-clone/static/js/2.e976ba49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85c66764f69da9ca7047",
+    "url": "/amazon-clone/static/js/main.97cd5293.chunk.js"
+  },
+  {
+    "revision": "e1ecdc36eb4d4da21497",
+    "url": "/amazon-clone/static/js/runtime-main.fc8c5de8.js"
+  }
+]);
